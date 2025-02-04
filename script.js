d80bf8f225a155185a7e258a869054a94df77c53
Peace Mbah <almapeace90@gmail.com>
2025-02-04 11:34:19 +0100

    new chages

---
+++ script.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const ul = document.querySelector("ul");
+
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active");
+    ul.classList.toggle("active");
+})
+
+document.querySelectorAll("a").forEach(n => n.
+    addEventListener("click", () =>{
+        hamburger.classList.remove("active");
+        ul.classList.remove("active");
+    })
+)
